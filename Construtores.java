3baba323625b1a6ec3e81e013459007e1f7b59a2
ITS-MATTI <2005matiasm@gmail.com>
2025-10-21 19:41:31 -0500

    Add files via upload

---
+++ Construtores.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package espe.edu.ec.construtores;
+
+/**
+ *
+ * @author pc
+ */
+public class Construtores {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
